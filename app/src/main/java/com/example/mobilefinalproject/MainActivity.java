af766fac44751f6b7075904053ba9e72a2cb2cb8
vonleswan <vaughan.c.alexander@gmail.com>
2019-10-28 22:49:48 -0400

    Added ToolBar for navigation to sub activities

--- app/src/main/java/com/example/mobilefinalproject/MainActivity.java
+++ app/src/main/java/com/example/mobilefinalproject/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.mobilefinalproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setTitle(null);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbarmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_news:
+                // User chose the "News" item, show the app news UI...
+                return true;
+
+            case R.id.action_currencies:
+                // User chose the "Currencies" action, show currencies UI ...
+                return true;
+
+            case R.id.action_recipes:
+                // User chose the "Recipes" action, show recipes UI ...
+                return true;
+
+            case R.id.action_stations:
+                // User chose the "Stations" action, show stations UI ...
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    public void startNews(View view){
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/mobilefinalproject/NewsActivity.java
@@ -0,0 +1,5 @@
+package com.example.mobilefinalproject;
+
+public class NewsActivity {
+
+}
