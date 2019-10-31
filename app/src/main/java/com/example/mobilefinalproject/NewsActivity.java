d51a2e5bc1c5105e93ef82066bd059116580cd12
vonleswan <vaughan.c.alexander@gmail.com>
2019-10-31 14:42:38 -0400

    Initial Master Commit

--- app/src/main/java/com/example/mobilefinalproject/NewsActivity.java
+++ app/src/main/java/com/example/mobilefinalproject/NewsActivity.java
@@ -1,5 +1,58 @@
 package com.example.mobilefinalproject;
 
-public class NewsActivity {
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+
+public class NewsActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setTitle(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbarmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_news:
+
+                return true;
+
+            case R.id.action_currencies:
+                // User chose the "Currencies" action, show currencies UI ...
+                return true;
+
+            case R.id.action_recipes:
+                // User chose the "Recipes" action, show recipes UI ...
+                return true;
+
+            case R.id.action_stations:
+                // User chose the "Stations" action, show stations UI ...
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
---
+++ app/src/main/java/com/example/mobilefinalproject/StationsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobilefinalproject;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+
+public class StationsActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stations);
+    }
+}
